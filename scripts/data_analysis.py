9b6da8497ce50b40d1154d510381beda4cda16ea
AhmedelGhani <ahmed.elghani04@gmail.com>
2025-02-23 09:23:11 +0100

    'for some reason, seaborn isnt imported properly which causes an error for me wh ile running. idk of this is a problem with the code or just how i've set it up. Can anyone of you try to run it in your own console to check if t he plot works properly?'

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -1,5 +1,9 @@
-import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
 
 def get_unique_users(df):
     return df['Id'].nunique()
@@ -13,4 +17,19 @@ def plot_total_distance(df):
     plt.xlabel('User Id')
     plt.ylabel('Total Distance')
     plt.title('Total Distance per unique user')
+    plt.show()
+
+def plot_calories_per_day(df, user_id, start_date=None, end_date=None):
+    df['ActivityDate']= pd.to_datetime(df['ActivityDate'])
+    user_data = df[df['Id']==user_id]
+    if start_date and end_date:
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        user_data = user_data[(user_data['ActivityDate']>= start_date) & (user_data['ActivityDate']<= end_date)]
+    plt.figure(figsize=(10,5))
+    sns.lineplot(x='ActivityDate', y='Calories', data=user_data)
+    plt.xlabel('Date')
+    plt.ylabel('Calories burned')
+    plt.title(f'Calories burned by user {user_id}')
+    plt.xticks(rotation=45)
     plt.show()
