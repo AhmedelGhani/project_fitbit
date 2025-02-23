44580009412fd4dba603d33bb22e94e077a31be2
AhmedelGhani <ahmed.elghani04@gmail.com>
2025-02-23 11:10:43 +0100

    plot margin adjustment

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 
-
 def get_unique_users(df):
     return df['Id'].nunique()
 
@@ -13,10 +12,12 @@ def compute_total_distance(df):
 
 def plot_total_distance(df):
     user_distance = compute_total_distance(df)
+    plt.figure(figsize=(10, 5))
     user_distance.set_index('Id')['TotalDistance'].plot(kind='bar')
     plt.xlabel('User Id')
     plt.ylabel('Total Distance')
     plt.title('Total Distance per unique user')
+    plt.subplots_adjust(bottom=0.3)
     plt.show()
 
 def plot_calories_per_day(df, user_id, start_date=None, end_date=None):
@@ -26,10 +27,11 @@ def plot_calories_per_day(df, user_id, start_date=None, end_date=None):
         start_date = datetime.strptime(start_date, "%Y-%m-%d")
         end_date = datetime.strptime(end_date, "%Y-%m-%d")
         user_data = user_data[(user_data['ActivityDate']>= start_date) & (user_data['ActivityDate']<= end_date)]
-    plt.figure(figsize=(9, 4))
+    plt.figure(figsize=(10, 5))
     sns.lineplot(x='ActivityDate', y='Calories', data=user_data)
     plt.xlabel('Date')
     plt.ylabel('Calories burned')
     plt.title(f'Calories burned by user {user_id}')
     plt.xticks(rotation=45)
+    plt.subplots_adjust(bottom=0.2)
     plt.show()
