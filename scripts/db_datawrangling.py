62661ecea871a951583197606afba9ae3ab6a491
MaximMn04 <maximmorales10@gmail.com>
2025-03-01 05:04:54 +0100

    Accessed database, outputted numbers of missing values

--- scripts/db_datawrangling.py
+++ scripts/db_datawrangling.py
@@ -1,1 +1,15 @@
 #Just to see if my branch works
+import sqlite3
+import pandas as pd
+import numpy as np
+
+
+conn = sqlite3.connect('fitbit_database.db')
+query = 'SELECT * FROM "weight_log";'
+df = pd.read_sql_query(query, conn)
+conn.close()
+
+print("Missing values before handling:")
+print(df.isnull().sum())
+
+
