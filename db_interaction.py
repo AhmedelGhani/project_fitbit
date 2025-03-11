042cd661bbfa49ec81c8212adbd3a936757e732f
AlecLiam <evers.alec@gmail.com>
2025-03-11 19:31:16 +0000

    Sorted the users by Id and class

--- db_interaction.py
+++ db_interaction.py
@@ -2,11 +2,25 @@ import sqlite3
 import pandas as pd
 import numpy as np
 
-connection=sqlite3.connect('fitbit_database.db')
-query='SELECT * FROM "daily_activity";'
+connection = sqlite3.connect('fitbit_database.db')
+query = 'SELECT Id FROM "daily_activity";'
 cursor = connection.cursor()
 cursor.execute(query)
-rows=cursor.fetchall()
+rows = cursor.fetchall()
 df = pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
-print(df)
-connection.close
+
+id_counts = df['Id'].value_counts().reset_index()
+id_counts.columns = ['Id', 'Count']
+def classify_user(count):
+    if count <= 10:
+        return 'Light user'
+    elif 11 <= count <= 15:
+        return 'Moderate user'
+    else:
+        return 'Heavy user'
+
+id_counts['Class'] = id_counts['Count'].apply(classify_user)
+user_class_df = id_counts[['Id', 'Class']]
+pd.set_option('display.float_format', '{:.0f}'.format)
+print(user_class_df)
+connection.close()
