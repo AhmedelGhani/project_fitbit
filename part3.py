7ccc3a2a6b3284603e1bd4d3b818a4b3f7b829d7
AlecLiam <evers.alec@gmail.com>
2025-03-13 13:22:46 +0000

    Made part 3.2, changed variable names to be more clear and added headers for every current and future question

--- part3.py
+++ part3.py
@@ -1,18 +1,20 @@
 import sqlite3
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
 
+# Part 3.1: Compute sleep duration per logId for each individual
 connection = sqlite3.connect('fitbit_database.db')
-query_activity = 'SELECT * FROM "daily_activity";'
+queryActivity = 'SELECT * FROM "daily_activity";'
 cursor = connection.cursor()
-cursor.execute(query_activity)
+cursor.execute(queryActivity)
 rows = cursor.fetchall()
-df_activity = pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
+activity = pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
 
-id_counts = df_activity['Id'].value_counts().reset_index()
-id_counts.columns = ['Id', 'Count']
+ids = activity['Id'].value_counts().reset_index()
+ids.columns = ['Id', 'Count']
 
-def classify_user(count):
+def classifyUser(count):
     if count <= 10:
         return 'Light user'
     elif 11 <= count <= 15:
@@ -20,17 +22,49 @@ def classify_user(count):
     else:
         return 'Heavy user'
 
-id_counts['Class'] = id_counts['Count'].apply(classify_user)
-user_class_df = id_counts[['Id', 'Class']]
+ids['Class'] = ids['Count'].apply(classifyUser)
+userClass = ids[['Id', 'Class']]
 
-query_sleep =  'SELECT * FROM "minute_sleep";'
-cursor.execute(query_sleep)
+querySleep =  'SELECT * FROM "minute_sleep";'
+cursor.execute(querySleep)
 rows = cursor.fetchall()
-df_sleep = pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
-sleep_duration_df = df_sleep.groupby(['Id', 'logId']).size().reset_index(name = 'Sleepduration')
+sleep = pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
+sleepDuration = sleep.groupby(['Id', 'logId']).size().reset_index(name = 'Sleepduration')
 
-merged_df = pd.merge(sleep_duration_df, user_class_df, on = 'Id', how = 'left')
+mergedClassSleep = pd.merge(userClass, sleepDuration, on = 'Id', how = 'left')
 
 pd.set_option('display.float_format', '{:.0f}'.format)
-print(merged_df)
+print(mergedClassSleep)
+
+
+# Part 3.2: Sleep duration compared to total active minutes with regression
+sleep['date'] = pd.to_datetime(sleep['date'])
+sleepPerDay = sleep.groupby(['Id', 'date']).size().reset_index(name='SleepMinutes')
+activity['ActivityDate'] = pd.to_datetime(activity['ActivityDate'])
+mergedActivitySleep = pd.merge(activity, sleepPerDay, left_on=['Id', 'ActivityDate'], right_on=['Id', 'date'], how='inner')
+mergedActivitySleep['TotalActiveMinutes'] = mergedActivitySleep['VeryActiveMinutes'] + mergedActivitySleep['FairlyActiveMinutes'] + mergedActivitySleep['LightlyActiveMinutes']
+
+x = mergedActivitySleep['TotalActiveMinutes']
+y = mergedActivitySleep['SleepMinutes']
+x = sm.add_constant(x)
+model = sm.OLS(y, x).fit()
+print(model.summary())
+
+
+# Part 3.3: Sleep duration compared to sedentary activity with regression and verification for normality
+
+
+
+# Part 3.4: Average steps, calories burned, and minutes of sleep per 4 hour time blocks
+
+
+
+# Part 3.5: Heart rate and total intensity plot for given Id
+
+
+
+# Part 3.6: Weather data analysis for Chicago
+
+
+
 connection.close()
