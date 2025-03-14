74dc772da5d6c000c7dcce999115c93252b54d39
MaximMn04 <maximmorales10@gmail.com>
2025-03-14 11:05:22 +0100

    Numeric_summary method modified so it can take into account  date range

--- scripts/db_datawrangling.py
+++ scripts/db_datawrangling.py
@@ -1,7 +1,6 @@
 import sqlite3
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 connection = sqlite3.connect('fitbit_database.db')
@@ -10,18 +9,18 @@ cursor = connection.cursor()
 cursor.execute(query)
 rows = cursor.fetchall()
 df = pd.DataFrame(rows, columns=[x[0] for x in cursor.description])
-print(df)
+#print(df)
 connection.close()
 
 print("Missing values before handling:")
-print(df.isnull().sum())
+#print(df.isnull().sum())
 
 df["WeightKg"] = df["WeightKg"].fillna(df["WeightPounds"] / 2.20462262185)
 df = df.drop(columns=['Fat']) 
 
-print("Missing values after handling:")
-print(df.isnull().sum())
-print(df)
+#print("Missing values after handling:")
+##print(df.isnull().sum())
+#print(df)
 
 def mergingtables(table1, table2, join_column='Id'):
     connection = sqlite3.connect('fitbit_database.db')
@@ -48,9 +47,18 @@ def mergingtables(table1, table2, join_column='Id'):
 merged_data = mergingtables('weight_log', 'hourly_steps')
 print(merged_data)
 
-def numeric_summary(df, columns=None):
+def numeric_summary(df, columns=None, start_date = None, end_date = None):
+    if 'Date' in df.columns:
+        df['Date'] = pd.to_datetime(df['Date'], infer_datetime_format = True)
+        if start_date is not None:
+            start_date = pd.to_datetime(start_date)
+            df = df[df['Date'] >= start_date]
+        if end_date is not None:
+            start_date = pd.to_datetime(end_date)
+            df = df[df['Date'] <= end_date]
+    
     if columns is None:
-        exclude = ['Id', 'LogId', 'Date', 'IsManualReport', 'ActivityHour']
+        exclude = ['Id', 'LogId', 'IsManualReport', 'ActivityHour']
         numeric_cols = df.select_dtypes(include=[np.number]).columns.tolist()
         columns = [col for col in numeric_cols if col not in exclude]
     else:
@@ -58,8 +66,9 @@ def numeric_summary(df, columns=None):
             columns = [columns]
         columns = [col for col in columns if col in df.columns]
     
+    print("Rows after date filter:", len(df))
     grouped_stats = df.groupby('Id')[columns].agg(['median', 'std', 'min', 'max', 'count'])
     return grouped_stats
 
-numeric_summary = numeric_summary (merged_data, 'StepTotal')
+numeric_summary = numeric_summary (merged_data, 'StepTotal', '4/6/2016', '4/7/2016')
 print (numeric_summary)
