7f59ed8bfdd5d7cd582aea4260a7b33e6974adfa
MaximMn04 <maximmorales10@gmail.com>
2025-03-17 09:40:27 +0100

    New boxplot function created

--- scripts/db_datawrangling.py
+++ scripts/db_datawrangling.py
@@ -2,6 +2,7 @@ import sqlite3
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
 from scipy.stats import linregress
 
 
@@ -143,6 +144,51 @@ def scatter_plot(df, xcol, ycol, ids= None, start_date = None, end_date = None, 
     plt.legend()
     plt.grid(True)
     plt.show()
+
+def box_plot (df, ids = None, columns = None, start_date = None, end_date = None, start_time = None, end_time = None, ):
+    df.rename(columns={'date': 'Date'}, inplace=True)
+    if 'Date' in df.columns:
+        df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %I:%M:%S %p')
+        if start_date is not None:
+            start_date = pd.to_datetime(start_date, format = '%m/%d/%Y')
+            df = df[df['Date'] >= start_date]
+        if end_date is not None:
+            end_date = pd.to_datetime(end_date, format = '%m/%d/%Y')
+            df = df[df['Date'] <= end_date]
+    
+    if start_time is not None:
+        start_time = pd.to_datetime(start_time, format = '%I:%M:%S %p').time()
+        df = df[df['Date'].dt.time >= start_time]
+    if end_time is not None:
+        end_time = pd.to_datetime(end_time, format = '%I:%M:%S %p').time()
+        df = df[df['Date'].dt.time <= end_time]
+    
+    if ids is not None:
+        if not isinstance (ids,list):
+            ids = [ids]
+        df = df[df['Id'].isin(ids)]
+    
+    if columns is None:
+        exclude = ['Id', 'LogId', 'IsManualReport', 'ActivityHour']
+        numeric_cols = df.select_dtypes(include=[np.number]).columns.tolist()
+        columns = [col for col in numeric_cols if col not in exclude]
+    else:
+        if not isinstance(columns, list):
+            columns = [columns]
+        columns = [col for col in columns if col in df.columns]
+
+    num_cols = len(columns)
+    fig, axes = plt.subplots(nrows = 1, ncols = num_cols, figsize = (6 * num_cols, 6), squeeze = False)
+    axes = axes.flatten()
+
+    for i, col in enumerate(columns):
+        sns.boxplot(x = 'Id', y = col, data = df, ax = axes[i])
+        axes[i].set_title(f"Boxplot of {col} by Id")
+        axes[i].set_xlabel("Id")
+        axes[i].set_ylabel(col)
+
+    plt.tight_layout()
+    plt.show()
 #Boxplot, scatterplot, over-time plot
 
 #temp = merged_data.copy()
@@ -166,3 +212,4 @@ merged_data['ActiveMinutes'] = merged_data['VeryActiveMinutes'] + merged_data['F
 numeric_summary = numeric_summary (merged_data, None, ['ActiveMinutes', 'SedentaryMinutes'], None, None, None, None)
 print (numeric_summary)
 scatter_plot(merged_data, 'SedentaryMinutes', 'ActiveMinutes', None, None, None, None, None)
+box_plot(merged_data, None, 'ActiveMinutes', None, None, None, None)
